e31141286bdd12d10201b612f25c51aa49cab028
robin <rogo009git@gmail.com>
2020-02-20 20:31:43 -0800

    update html and style

---
+++ script.js
@@ -0,0 +1,17 @@
+function filter() {
+    let FilterValue, input, ul, li, i;
+
+    input = document.getElementById('search');
+    FilterValue = input.value().toUpperCase();
+    ul = document.getElementById('menu');
+    li = ul.getElementsByTagName('li');
+
+    for(i=0; i<li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(FilterValue) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "";
+        }
+    }
+}
