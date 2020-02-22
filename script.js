3e976455a7e278a548ed5a71fdee7f55b6019255
robin <rogo009git@gmail.com>
2020-02-21 22:11:39 -0800

    update index and script files

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function filter() {
-    let FilterValue, input, ul, li, i;
+// target input field
 
-    input = document.getElementById('search');
-    FilterValue = input.value().toUpperCase();
-    ul = document.getElementById('menu');
-    li = ul.getElementsByTagName('li');
+const filterInput = document.getElementById('filterInput');
 
-    for(i=0; i<li.length; i++) {
-        let a = li[i].getElementsByTagName('a')[0];
-        if(a.innerHTML.toUpperCase().indexOf(FilterValue) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "";
-        }
-    }
+// event listener
+
+filterInput.addEventListener('keyup', filterNames);
+
+// function
+
+function filterNames() {
+    // input value transformed to uppercase
+    const filterValue = document.getElementById('filterInput').value.toUpperCase();
+    console.log(filterValue);
+
+    // target ul and id 'names'
+    const ul = document.getElementById('names');
+
+    // target li items from the ul variable
+    let li = ul.querySelectorAll('li.collection-item');
 }
+
